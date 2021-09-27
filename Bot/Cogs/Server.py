c2fe77bd09d531d42632f90e901b6d4dc9391762
Jaehun Kim <wogns3623@gmail.com>
2021-09-27 21:25:13 +0900

    Add logging function

--- Bot/Cogs/Server.py
+++ Bot/Cogs/Server.py
@@ -1,57 +1,67 @@
-import discord
+import sys
 import os
 import errno
+
+import discord
 from discord.ext import commands
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from . import Utils
 
 INP_PIPE_DIR = "/home/smdb/ipipe"
 OUT_PIPE_DIR = "/home/smdb/opipe"
-
 BUF_SIZE = 1024
 
 
 class Server(commands.Cog):
-  def __init__(self, app):
-    self.app = app
+    def __init__(self, app):
+        self.app = app
 
-  @commands.command(name="reboot", help="x분 후에 서버 재부팅")
-  async def reboot_server(self, ctx, time: int):
-    response = f"Server will reboot in {time} minute."
-    await ctx.send(response)
+    @commands.command(name="reboot", help="x분 후에 서버 재부팅")
+    async def reboot_server(self, ctx, time: int):
+        response = f"Server will reboot in {time} minute."
+        await ctx.send(response)
 
-  @commands.command(name="console", help="direct console input")
-  async def console(self, ctx, *, command):
-    if not os.path.exists(INP_PIPE_DIR):
-      try:
-        os.mkfifo(INP_PIPE_DIR)
-      except OSError as oe: 
-        if oe.errno != errno.EEXIST:
-          print(f"[Bot/Error] mkfifo fail, errno is {oe.errno}")
-          await ctx.send("오류가 발생했습니다")
-          return
+    @commands.command(name="console", help="direct console input")
+    async def console(self, ctx, *, command):
+        if not os.path.exists(INP_PIPE_DIR):
+            try:
+                os.mkfifo(INP_PIPE_DIR)
+            except OSError as oe:
+                if oe.errno != errno.EEXIST:
+                    errlog(f"mkfifo fail, errno is {oe.errno}")
+                    await ctx.send("오류가 발생했습니다")
+                    return
 
-    with open(INP_PIPE_DIR, "w") as fifo:
-      if fifo.write(f"/{command}") != len(command) + 1:
-        response = "서버에 명령을 전송하는 도중 오류가 발생했습니다."
-      response = "서버에 명령을 전송했습니다."
+        with open(INP_PIPE_DIR, "w") as fifo:
+            log(f"Send command to server:\n{command}")
+            if fifo.write(f"/{command}") != len(command) + 1:
+                response = "서버에 명령을 전송하는 도중 오류가 발생했습니다."
+                errlog(f"Fail to send command to server {oe.errno}")
+            response = "서버에 명령을 전송했습니다."
 
-      await ctx.send(response)
+            await ctx.send(response)
 
-  @commands.command(name="log", help="direct console output\nRead N lines from console\n Default 1 line")
-  async def console(self, ctx, *, n=1):
-    if not os.path.exists(OUT_PIPE_DIR):
-      try:
-        os.mkfifo(OUT_PIPE_DIR)
-      except OSError as oe: 
-        if oe.errno != errno.EEXIST:
-          print(f"[Bot/Error] mkfifo fail, errno is {oe.errno}")
-          await ctx.send("오류가 발생했습니다")
-          return
+    @commands.command(
+        name="log",
+        help="direct console output\nRead N lines from console\n Default 1 line",
+    )
+    async def console(self, ctx, *, n=1):
+        if not os.path.exists(OUT_PIPE_DIR):
+            try:
+                os.mkfifo(OUT_PIPE_DIR)
+            except OSError as oe:
+                if oe.errno != errno.EEXIST:
+                    errlog(f"mkfifo fail, errno is {oe.errno}")
+                    await ctx.send("오류가 발생했습니다")
+                    return
 
-    with open(OUT_PIPE_DIR, "r") as fifo:
-      for i in range(n):
-        res = fifo.readline(BUF_SIZE)
-        await ctx.send(res)
+        with open(OUT_PIPE_DIR, "r") as fifo:
+            for i in range(n):
+                res = fifo.readline(BUF_SIZE)
+                log(f"Send message to guild:\n{res}")
+                await ctx.send(res)
 
 
 def setup(app):
-  app.add_cog(Server(app))
+    app.add_cog(Server(app))
--- Bot/Main.py
+++ Bot/Main.py
@@ -1,13 +1,15 @@
-import os
 import sys
+import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
-from discord.ext import commands
+
+import Utils
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv("DISCORD_TOKEN")
 # TOKEN = os.getenv('TEST_TOKEN')
 
 app = commands.Bot(command_prefix="!")
@@ -16,24 +18,21 @@ for filename in os.listdir("Cogs"):
     if filename.endswith(".py"):
         app.load_extension(f"Cogs.{filename[:-3]}")
 
-def print_log(*args):
-    print(*args, file=sys.stderr)
-
 
 @app.event
 async def on_ready():
-    print_log(f'{app.user} has connected to Discord!')
-    print_log(f'{app.user.name}\n{app.user.id}\n=============')
+    print_log(f"{app.user} has connected to Discord!")
+    print_log(f"{app.user.name}\n{app.user.id}\n=============")
     await app.change_presence(
-        status=discord.Status.online,
-        activity=discord.Game("서버 관리")
+        status=discord.Status.online, activity=discord.Game("서버 관리")
     )
 
 
 @app.command(
     name="reload",
-    help="Reload certain extension.\nUsing !reload {extension} to reload {extension}.\ntype nothing or \"all\" will reload all extensions",
-    usage="!reload {extension}\n!reload all\n!reload")
+    help='Reload certain extension.\nUsing !reload {extension} to reload {extension}.\ntype nothing or "all" will reload all extensions',
+    usage="!reload {extension}\n!reload all\n!reload",
+)
 async def reload_commands(ctx, extension=None):
     if extension is None or extension == "all":
         await ctx.send(":white_check_mark: Reloading all extensions...")
@@ -48,13 +47,16 @@ async def reload_commands(ctx, extension=None):
         app.load_extension(f"Cogs.{extension}")
         await ctx.send(f":white_check_mark: Reload {extension}!")
 
-@app.command(name='react', help='이모지 달기')
+
+@app.command(name="react", help="이모지 달기")
 async def react_to_emoji(ctx, emoji):
-    print_log(emoji)
+    log(emoji)
     await ctx.message.add_reaction(emoji)
 
-@app.command(name='stop', help='봇 멈추기')
+
+@app.command(name="stop", help="봇 멈추기")
 async def stopBot(ctx):
     quit()
+
 
 app.run(TOKEN)
