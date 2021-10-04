2171b881e9e6f61b41e4531bb2e8225112bf9e3d
Jaehun Kim <wogns3623@gmail.com>
2021-10-04 21:04:45 +0900

    Add load & unload function

--- Bot/Main.py
+++ Bot/Main.py
@@ -10,7 +10,6 @@ from Utils import log, errlog
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-# TOKEN = os.getenv('TEST_TOKEN')
 
 app = commands.Bot(command_prefix="!")
 
@@ -29,22 +28,54 @@ async def on_ready():
 
 
 @app.command(
+    name="load",
+    help='load certain extension.\nUsing !load {extension} to load {extension}.\ntype nothing or "all" will load all extensions',
+    usage="!load {extension}\n!load all\n!load",
+)
+async def load_commands(ctx, extension=None):
+    if extension is None or extension == "all":
+        await ctx.send("loading all extensions...")
+        for filename in os.listdir("Cogs"):
+            if filename.endswith(".py"):
+                app.load_extension(f"Cogs.{filename[:-3]}")
+        await ctx.send(":white_check_mark: load all extensions!")
+    else:
+        await ctx.send(f"loading {extension}...")
+        app.load_extension(f"Cogs.{extension}")
+        await ctx.send(f":white_check_mark: load {extension}!")
+
+@app.command(
+    name="unload",
+    help='unload certain extension.\nUsing !unload {extension} to unload {extension}.\ntype nothing or "all" will unload all extensions',
+    usage="!unload {extension}\n!unload all\n!unload",
+)
+async def unload_commands(ctx, extension=None):
+    if extension is None or extension == "all":
+        await ctx.send("Unloading all extensions...")
+        for filename in os.listdir("Cogs"):
+            if filename.endswith(".py"):
+                app.unload_extension(f"Cogs.{filename[:-3]}")
+        await ctx.send(":white_check_mark: Unload all extensions!")
+    else:
+        await ctx.send(f"Unloading {extension}...")
+        app.unload_extension(f"Cogs.{extension}")
+        await ctx.send(f":white_check_mark: Unload {extension}!")
+
+@app.command(
     name="reload",
     help='Reload certain extension.\nUsing !reload {extension} to reload {extension}.\ntype nothing or "all" will reload all extensions',
     usage="!reload {extension}\n!reload all\n!reload",
 )
 async def reload_commands(ctx, extension=None):
     if extension is None or extension == "all":
-        await ctx.send(":white_check_mark: Reloading all extensions...")
+        await ctx.send("Reloading all extensions...")
         for filename in os.listdir("Cogs"):
             if filename.endswith(".py"):
-                app.unload_extension(f"Cogs.{filename[:-3]}")
-                app.load_extension(f"Cogs.{filename[:-3]}")
-                await ctx.send(":white_check_mark: Reload all extensions!")
+                app.reload_extension(f"Cogs.{filename[:-3]}")
+        await ctx.send(":white_check_mark: Reload all extensions!")
     else:
-        await ctx.send(f":white_check_mark: Reloading {extension}...")
-        app.unload_extension(f"Cogs.{extension}")
-        app.load_extension(f"Cogs.{extension}")
+        await ctx.send(f"Reloading {extension}...")
+        app.reload_extension(f"Cogs.{extension}")
         await ctx.send(f":white_check_mark: Reload {extension}!")
 
 
